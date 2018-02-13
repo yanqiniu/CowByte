917dbcc49749b70a862b7ae1bf3606c14de21185
A calm cow <yanqi.roy.niu@gmail.com>
2018-02-12 21:13:52 -0800

    UObject skeleton.

--- src/CowByteEngine/UObject.cpp
+++ src/CowByteEngine/UObject.cpp
@@ -1,12 +1,31 @@
 #include "UObject.h"
 
+int UObject::m_ObjectAmount = 0;
+int UObject::m_IDCounter = 0;
 
+UObject::UObject(const std::tstring name) :
+    m_Name(name),
+    m_ID(m_IDCounter),
 
-UObject::UObject()
+    m_bIsInitialized(false),
+    m_bIsPostInitialized(false),
+    m_bIsContentLoaded(false),
+    m_bIsPostContentLoaded(false),
+    m_bIsDestroyed(false),
+
+    m_bCanDraw(true),
+    m_bCanTick(true)
 {
+    ++m_ObjectAmount;
+    ++m_IDCounter;
 }
-
 
 UObject::~UObject()
 {
+    --m_ObjectAmount;
+}
+
+void UObject::Reset()
+{
+
 }
--- src/CowByteEngine/UObject.h
+++ src/CowByteEngine/UObject.h
@@ -1,10 +1,65 @@
 #ifndef _UOBJECT_H
 #define _UOBJECT_H
+
+#include "context.h"
+#include "string.h"
+
 class UObject
 {
 public:
-	UObject();
-	virtual ~UObject();
+    UObject(const std::tstring name);
+    virtual ~UObject();
+
+    //void* operator new(size_t size);
+    //void operator delete(void* pDelete);
+
+    virtual bool Initialize()                 { m_bIsInitialized = true; return true; }
+    virtual bool PostInitialize()             { m_bIsPostInitialized = true; return true; }
+    virtual bool LoadContent()                { m_bIsContentLoaded = true; return true; }
+    virtual bool PostLoadContent()            { m_bIsPostContentLoaded = true; return true; }
+    virtual bool Update()                     { return true; }
+    //virtual bool LateUpdate();
+    virtual bool Draw()                       { return true; }
+    virtual bool DrawUI()                     { return true; }
+    virtual bool ShutDown()                   { return true; }
+
+    virtual void Reset();
+
+    void SetName(std::tstring& name) { m_Name = name; }
+    const std::tstring& GetName() { return m_Name; }
+
+    bool IsInitialized() const       { return m_bIsInitialized; }
+    bool IsPostInitialized() const   { return m_bIsPostInitialized; }
+    bool IsContentLoaded() const     { return m_bIsContentLoaded; }
+    bool IsPostContentLoaded() const { return m_bIsPostContentLoaded; }
+
+    void SetCanTick(bool canTick) { m_bCanTick = canTick; }
+    bool CanTick() const { return m_bCanTick; }
+    void SetCanDraw(bool canDraw) { m_bCanDraw = canDraw; }
+    bool CanDraw() const { return CanDraw(); }
+
+    void Destroy() { m_bIsDestroyed = true; }
+    bool IsDestroyed() const { return m_bIsDestroyed; }
+
+protected:
+
+    static int m_ObjectAmount;
+    static int m_IDCounter;
+    int m_ID;
+
+    bool m_bIsInitialized;
+    bool m_bIsPostInitialized;
+
+    bool m_bIsContentLoaded;
+    bool m_bIsPostContentLoaded;
+
+    std::tstring m_Name;
+
+private:
+    bool m_bIsDestroyed;
+
+    bool m_bCanTick;
+    bool m_bCanDraw;
 };
 
 #endif // ifndef _UOBJECT_H
