3442b0dff1246b5236323e35aad3bd72e26327e0
A calm cow <yanqi.roy.niu@gmail.com>
2018-02-25 19:33:03 -0800

    Extra include for DX11. Some graphics header stuff.

--- src/CowByteEngine/Graphics.h
+++ src/CowByteEngine/Graphics.h
@@ -3,7 +3,13 @@
 
 #include <windows.h>
 #include <windowsx.h>
+#include <d3d11.h>
+#include <D3DX10.h>
+#include <D3DX11.h>
 
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
 
 #include "System.h"
 #include "Window.h"
@@ -24,6 +30,10 @@ public:
     virtual bool ShutDown();
 
 private:
+    ID3D11Device *m_Device;
+    ID3D11DeviceContext *m_DeviceContext;
+    IDXGISwapChain *m_SwapChain;
+
 
 
 };
