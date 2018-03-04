abfef7d8106d62579c9c9ce544f42ba2238dd33d
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-03 21:53:46 -0800

    Attempt fixing vec3 matrix4x4 circular dependency,

--- src/Tests/Test.cpp
+++ src/Tests/Test.cpp
@@ -26,12 +26,22 @@ int main()
     //    }
     //}
 
-    Vec3 testVec(12.0f, 3.0f, 7.0f, 33.1f);
+    //Vec3 testVec(12.0f, 3.0f, 7.0f);
 
-    float data[16] = { 1.0f, 5.0f, 9.0f, 13.0f, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16 };
-    Matrix4x4 testMatrix(data);
+    //float data[16] = { 1.0f, 5.0f, 9.0f, 13.0f, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16 };
+    Matrix4x4 testMatrix = Matrix4x4::Identity();//(data);
+    ////testMatrix.Transpose();
+    //Vec3 newVec = testVec * testMatrix;
 
-    Vec3 newVec = testVec * testMatrix;
+    //for (size_t row = 0; row < 4; ++row)
+    //{
+    //    for (size_t col = 0; col < 4; ++col)
+    //    {
+    //        printf("%f, ", testMatrix.At(row, col));
+    //    }
+    //    printf("\n");
+    //}
+    //testVec = testVec.Mul(Matrix4x4::Translate(3.0f, 2.0f, 1.0f));
     
     //////////////////////////////////////////////////////////////////////////
     double duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
