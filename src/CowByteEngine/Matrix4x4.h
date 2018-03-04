bf1f13b6dcbde4d26e68492cf15a6e591d0adeb4
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-03 23:14:29 -0800

    Resolved Vec3/Matrix4x4 circular dependency.

--- src/CowByteEngine/Matrix4x4.h
+++ src/CowByteEngine/Matrix4x4.h
@@ -21,24 +21,81 @@ __declspec(align(16)) struct Matrix4x4
 {
     __m128 _data[4]; // 4 columns, row vector
 
+#pragma region Con/Destructor, Getter/Setter
+
     // Default constructor that does nothing.
-    Matrix4x4();
+    __forceinline Matrix4x4()
+    {
+    }
+
     // Row major constructor.
-    Matrix4x4(float e11, float e12, float e13, float e14,
-                            float e21, float e22, float e23, float e24,
-                            float e31, float e32, float e33, float e34,
-                            float e41, float e42, float e43, float e44);
-    Matrix4x4(float rows[4][4]);
+    __forceinline Matrix4x4(float e11, float e12, float e13, float e14,
+        float e21, float e22, float e23, float e24,
+        float e31, float e32, float e33, float e34,
+        float e41, float e42, float e43, float e44)
+    {
+        _data[0] = _mm_setr_ps(e11, e21, e31, e41);
+        _data[1] = _mm_setr_ps(e12, e22, e32, e42);
+        _data[2] = _mm_setr_ps(e13, e23, e33, e43);
+        _data[3] = _mm_setr_ps(e14, e24, e34, e44);
+    }
+
+    __forceinline Matrix4x4(float rows[4][4]) :
+        Matrix4x4(rows[1][1], rows[1][2], rows[1][3], rows[1][4],
+            rows[2][1], rows[2][2], rows[2][3], rows[2][4],
+            rows[3][1], rows[3][2], rows[3][3], rows[3][4],
+            rows[4][1], rows[4][2], rows[4][3], rows[4][4])
+    {
+
+    }
+
     // Row major list constructor. This constructor takes 
     // a pointer so it's not as safe, but it's efficient.
-    Matrix4x4(float *e);
+    __forceinline Matrix4x4(float *e)
+    {
+        _data[0] = _mm_setr_ps(e[0], e[4], e[8], e[12]);
+        _data[1] = _mm_setr_ps(e[1], e[5], e[9], e[13]);
+        _data[2] = _mm_setr_ps(e[2], e[6], e[10], e[14]);
+        _data[3] = _mm_setr_ps(e[3], e[7], e[11], e[15]);
+    }
 
-    float At(size_t row, size_t col);
-    void Transpose();
+    __forceinline float At(size_t row, size_t col)
+    {
+        return _data[col].m128_f32[row];
+    }
 
-    static Matrix4x4 Identity();
-    static Matrix4x4 Translate(float x, float y, float z);
+#pragma endregion
+
+#pragma region Common Operations
+
+    void Transpose()
+    {
+        _MM_TRANSPOSE4_PS(_data[0], _data[1], _data[2], _data[3]);
+    }
+
+#pragma endregion
+
+#pragma region Useful Matrices
+
+    __forceinline static Matrix4x4 Identity()
+    {
+        return Matrix4x4(1.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    __forceinline static Matrix4x4 Translate(float x, float y, float z)
+    {
+        return Matrix4x4(1.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f, 0.0f,
+            x, y, z, 1.0f);
+    }
+
     static Matrix4x4 Translate(Vec3 *vec);
+
+#pragma endregion
 };
 
 
--- src/CowByteEngine/Vec3.h
+++ src/CowByteEngine/Vec3.h
@@ -3,66 +3,200 @@
 #include <xmmintrin.h>
 #include <smmintrin.h>
 #include <exception>
-
-class Matrix4x4;
+#include "Matrix4x4.h"
 
 __declspec(align(16)) struct Vec3
 {
     __m128 _data;
-    Vec3();
+
+#pragma region Con/Destructor, Getter/Setter
 
     // W set to 1.0f, representing a point.
-    Vec3(float xIn, float yIn, float zIn);
-    Vec3(float xIn, float yIn, float zIn, float wIn);
-    Vec3(const __m128 &data);
+    __forceinline Vec3(float xIn, float yIn, float zIn)
+    {
+        _data = _mm_setr_ps(xIn, yIn, zIn, 1.0f);
+    }
+    __forceinline Vec3(float xIn, float yIn, float zIn, float wIn)
+    {
+        _data = _mm_setr_ps(xIn, yIn, zIn, wIn);
+    }
+    __forceinline Vec3(const __m128 &data)
+    {
+        _data = data;
+    }
+
     ~Vec3() {};
 
     // ! Access individual element from __m128, slow.
-    float X() const;
-    float Y() const;
-    float Z() const;
-    float W() const;
-    void Set(float xIn, float yIn, float zIn, float wIn);
+    __forceinline float X() const { return _data.m128_f32[0]; }
+    __forceinline float Y() const { return _data.m128_f32[1]; }
+    __forceinline float Z() const { return _data.m128_f32[2]; }
+    __forceinline float W() const { return _data.m128_f32[3]; }
 
-    void operator=(const Vec3 &rhs);
-    Vec3 operator+(const Vec3 &rhs);
-    Vec3 operator-(const Vec3 &rhs);
-    Vec3 operator/(float divisor);
+    __forceinline void Set(float xIn, float yIn, float zIn, float wIn)
+    {
+        _data = _mm_setr_ps(xIn, yIn, zIn, wIn);
+    }
+#pragma endregion
 
-    void operator+=(const Vec3 &rhs);
-    void operator-=(const Vec3 &rhs);
-    void operator*=(float scalar);
-    void operator/=(float divisor);
+#pragma region Common operators
+    __forceinline void operator=(const Vec3 &rhs)
+    {
+        _data = rhs._data;
+    }
+    __forceinline Vec3 operator+(const Vec3 &rhs)
+    {
+        return Vec3(_mm_add_ps(_data, rhs._data));
+    }
+    __forceinline Vec3 operator-(const Vec3 &rhs)
+    {
+        return Vec3(_mm_sub_ps(_data, rhs._data));
+    }
+    __forceinline Vec3 operator/(float divisor)
+    {
+        if (divisor == 0)
+        {
+            throw(std::exception("Vec3: divisor is zero!"));
+        }
+        __m128 temp = _mm_setr_ps(divisor, divisor, divisor, 1.0f);
+        return Vec3(_mm_div_ps(_data, temp));
+    }
 
-    void Add(const Vec3 &toAdd);
-    void Scale(float scalar);
+
+    __forceinline void operator+=(const Vec3 &rhs)
+    {
+        _data = _mm_add_ps(_data, rhs._data);
+    }
+    __forceinline void operator-=(const Vec3 &rhs)
+    {
+        _data = _mm_sub_ps(_data, rhs._data);
+    }
+    __forceinline void operator*=(float scalar)
+    {
+        __m128 temp = _mm_setr_ps(scalar, scalar, scalar, 1.0f);
+        _data = _mm_mul_ps(_data, temp);
+    }
+    __forceinline void operator/=(float divisor)
+    {
+        if (divisor == 0)
+        {
+            throw(std::exception("Vec3: divisor is zero!"));
+        }
+        __m128 temp = _mm_setr_ps(divisor, divisor, divisor, 1.0f);
+        _data = _mm_div_ps(_data, temp);
+    }
+#pragma endregion
+
+#pragma region Common operations
+    __forceinline void Add(const Vec3 &toAdd)
+    {
+        _data = _mm_add_ps(_data, toAdd._data);
+    }
+    __forceinline void Scale(float scalar)
+    {
+        (*this) *= scalar;
+    }
 
     // Squared length of the vector.
-    float SqLen();
-    // Length of the vector.
-    float Len();
+    __forceinline float SqLen()
+    {
+        __m128 temp = _mm_dp_ps(_data, _data, 0b01110001);
+        return temp.m128_f32[0];
+    }
+
+    // length of the vector.
+    __forceinline float Len()
+    {
+        return _mm_sqrt_ss(_mm_dp_ps(_data, _data, 0b01110001)).m128_f32[0];
+    }
+
     // Test to see if it's a zero vector (x, y, z are zero)
-    bool IsZero();
+    __forceinline bool IsZero()
+    {
+        // Using dot product instead of checking x, y and z individually
+        // since the later one introduces around 60% more time according 
+        // to testing.
+        return _mm_dp_ps(_data, _data, 0b01110001).m128_f32[0] == 0;
+    }
+
     // Set W to 1.
-    void ToPoint();
+    __forceinline void ToPoint()
+    {
+        _data = _mm_insert_ps(_data, _mm_set_ps1(1.0f), 0b11110000);
+    }
+
     // Set W to 0.
-    void ToDir();
+    __forceinline void ToDir()
+    {
+        _data = _mm_insert_ps(_data, _mm_set_ps1(0.0f), 0b11110000);
+    }
+
     // Normalize the vector. Notice that since only Vector
     // can be normalized, W component gets set to 0.
-    void Normalize();
+    __forceinline void Normalize()
+    {
+        __m128 temp = _mm_dp_ps(_data, _data, 0b01110111);
+        temp = _mm_rsqrt_ps(temp);
+        temp = _mm_insert_ps(temp, _mm_set_ps1(0.0f), 0b11110000); // set w of temp to 0.0f
+        _data = _mm_mul_ps(_data, temp);
+    }
     // Return a normalized version of the Vec3.
     // Notice this DOES NOT modify _data.
-    Vec3 Normalized();
-    float Dot(const Vec3 &toDot);
+    __forceinline Vec3 Normalized()
+    {
+        __m128 temp = _mm_dp_ps(_data, _data, 0b01110111);
+        temp = _mm_rsqrt_ps(temp);
+        temp = _mm_insert_ps(temp, _mm_set_ps1(0.0f), 0b11110000); // set w of temp to 0.0f
+        return Vec3(_mm_mul_ps(_data, temp));
+    }
+
+    // Dot product.
+    __forceinline float Dot(const Vec3 &toDot)
+    {
+        return _mm_dp_ps(_data, toDot._data, 0b01110001).m128_f32[0];
+    }
+
     // Return cross vector. Does not change Data.
-    Vec3 Cross(const Vec3 &toCross);
-    Vec3 Mul(const Matrix4x4 &matrix);
-    Vec3 operator*(const Matrix4x4 &matrix);
+    __forceinline Vec3 Cross(const Vec3 &toCross)
+    {
+        __m128 temp0 = _mm_shuffle_ps(_data, _data, _MM_SHUFFLE(3, 0, 2, 1));
+        __m128 temp1 = _mm_shuffle_ps(toCross._data, toCross._data, _MM_SHUFFLE(3, 1, 0, 2));
+        __m128 result = _mm_mul_ps(temp0, temp1);
+
+        temp0 = _mm_shuffle_ps(_data, _data, _MM_SHUFFLE(3, 1, 0, 2));
+        temp1 = _mm_shuffle_ps(toCross._data, toCross._data, _MM_SHUFFLE(3, 0, 2, 1));
+        return Vec3(_mm_sub_ps(result, _mm_mul_ps(temp0, temp1)));
+    }
+#pragma endregion
+
+#pragma region Matrix4x4 operators and operations
+
+    __forceinline Vec3 Mul(const Matrix4x4 &matrix)
+    {
+        __m128 temp = _mm_dp_ps(_data, matrix._data[0], 0b11110001); // x
+        temp = _mm_insert_ps(temp, _mm_dp_ps(_data, matrix._data[1], 0b11110001), 0b00010000); // y
+        temp = _mm_insert_ps(temp, _mm_dp_ps(_data, matrix._data[2], 0b11110001), 0b00100000); // z
+        temp = _mm_insert_ps(temp, _mm_dp_ps(_data, matrix._data[3], 0b11110001), 0b00110000); // w
+
+        return Vec3(temp);
+    }
+    __forceinline Vec3 operator*(const Matrix4x4 &matrix)
+    {
+        return Mul(matrix);
+    }
+
+#pragma endregion
     
 };
 
-Vec3 operator*(const Vec3& vec3, float scalar);
-Vec3 operator*(float scalar, const Vec3& vec3);
-
+__forceinline Vec3 operator*(const Vec3& vec3, float scalar)
+{
+    __m128 temp = _mm_setr_ps(scalar, scalar, scalar, 1.0f);
+    return Vec3(_mm_mul_ps(vec3._data, temp));
+}
+__forceinline Vec3 operator*(float scalar, const Vec3& vec3)
+{
+    return vec3 * scalar;
+}
 
 #endif
