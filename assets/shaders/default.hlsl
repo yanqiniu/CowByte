bd8653ddebb5f8cc09c07c16ae9192e07b07c4b8
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-13 00:57:51 -0700

    Test assets update.

--- assets/shaders/default.hlsl
+++ assets/shaders/default.hlsl
@@ -4,6 +4,22 @@ struct VS_Output
     float4 color : COLOR;
 };
 
+
+cbuffer PerApplication : register(b0)
+{
+    matrix projectionMatrix;
+}
+
+cbuffer PerFrame : register(b1)
+{
+    matrix cameraViewMatrix;
+}
+
+cbuffer PerObject : register(b2)
+{
+    matrix worldMatrix;
+}
+
 VS_Output VShader(float4 position : POSITION, float4 color : COLOR)
 {
     VS_Output output;
