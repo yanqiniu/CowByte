f2169ec5a41e262d146482e305909a1416777239
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-13 00:58:50 -0700

    Graphics pipeline setup update.

--- src/CowByteEngine 0.2/Render/Graphics.cpp
+++ src/CowByteEngine 0.2/Render/Graphics.cpp
@@ -35,7 +35,15 @@ Graphics::Graphics(const GraphicsData &data):
     m_pDevice(nullptr),
     m_pDeviceContext(nullptr),
     m_pSwapChain(nullptr),
-    m_pMeshManager(nullptr)
+    m_pMeshManager(nullptr),
+    m_pRenderTarget(nullptr),
+    m_pDepthStencilView(nullptr),
+    m_pVertexBuffer(nullptr),
+    m_pConstantBuffers(),
+    m_pDepthStencilBuffer(nullptr),
+    m_pInputLayout(nullptr),
+    m_pDepthStencilState(nullptr),
+    m_pRasterizerState(nullptr)
 {
 
 }
@@ -46,6 +54,12 @@ Graphics::~Graphics()
 
 bool Graphics::Initialize()
 {
+    // Create mesh manager.
+    m_pMeshManager = new MeshManager();
+    // TEST: Load single cube into mesh manager.
+    m_pMeshManager->CPULoadMesh("cube.mesha");
+    m_pMeshManager->CPULoadMesh("cube.mesha");
+
     if (m_pWindow == nullptr)
     {
         DbgERROR("Graphics: Window pointer null!");
@@ -65,10 +79,6 @@ bool Graphics::Initialize()
         return false;
     }
 
-    // Create mesh manager.
-    m_pMeshManager = new MeshManager();
-    // TEST: Load single cube into mesh manager.
-    m_pMeshManager->CPULoadMesh("cube.mesha");
 
     return true;
 }
@@ -114,6 +124,10 @@ bool Graphics::InitializePipeline()
     swapDesc.BufferCount = NumOfBuffers;
     swapDesc.BufferDesc.Width = m_pWindow->GetWidth();
     swapDesc.BufferDesc.Height = m_pWindow->GetHeight();
+    DXGI_RATIONAL refreshRate;
+    refreshRate.Numerator = 60;
+    refreshRate.Denominator = 1;
+    swapDesc.BufferDesc.RefreshRate = refreshRate;
     swapDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
     swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
     swapDesc.OutputWindow = m_pWindow->GetWindowHandle();
@@ -121,18 +135,29 @@ bool Graphics::InitializePipeline()
     swapDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
     swapDesc.Windowed = !m_pWindow->GetIsFullScreen();
 
+    D3D_FEATURE_LEVEL featureLevels[] =
+    {
+        D3D_FEATURE_LEVEL_11_0,
+        D3D_FEATURE_LEVEL_10_1,
+        D3D_FEATURE_LEVEL_10_0,
+        D3D_FEATURE_LEVEL_9_3,
+        D3D_FEATURE_LEVEL_9_2,
+        D3D_FEATURE_LEVEL_9_1
+    };
+    D3D_FEATURE_LEVEL featureLevel;
+
     ThrowIfFailed(D3D11CreateDeviceAndSwapChain(
-        NULL,
+        nullptr,
         D3D_DRIVER_TYPE_HARDWARE,
-        NULL,
-        NULL,
-        NULL,
-        NULL,
+        nullptr,
+        0,
+        featureLevels,
+        _countof(featureLevels),
         D3D11_SDK_VERSION,
         &swapDesc,
         &m_pSwapChain,
         &m_pDevice,
-        NULL,
+        &featureLevel,
         &m_pDeviceContext
     ));
 
@@ -140,16 +165,59 @@ bool Graphics::InitializePipeline()
     // Set render target.
     ID3D11Texture2D *pBackBuffer;
     ThrowIfFailed(m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer));
-    ThrowIfFailed(m_pDevice->CreateRenderTargetView(pBackBuffer, NULL, &m_pRenderTarget));
+    ThrowIfFailed(m_pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &m_pRenderTarget));
     pBackBuffer->Release();
-    m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTarget, NULL);
+    m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTarget, nullptr);
+
+    // Create a depth stencil buffer and view.
+    D3D11_TEXTURE2D_DESC depthStencilBufferDesc;
+    ZeroMemory(&depthStencilBufferDesc, sizeof(D3D11_TEXTURE2D_DESC));
+    depthStencilBufferDesc.ArraySize = 1;
+    depthStencilBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+    depthStencilBufferDesc.CPUAccessFlags = 0;
+    depthStencilBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    depthStencilBufferDesc.Width = m_pWindow->GetWidth();
+    depthStencilBufferDesc.Height = m_pWindow->GetHeight();
+    depthStencilBufferDesc.MipLevels = 1;
+    depthStencilBufferDesc.SampleDesc.Count = 4;
+    depthStencilBufferDesc.SampleDesc.Quality = 0;
+    depthStencilBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+    ThrowIfFailed(m_pDevice->CreateTexture2D(&depthStencilBufferDesc, nullptr, &m_pDepthStencilBuffer));
+    ThrowIfFailed(m_pDevice->CreateDepthStencilView(m_pDepthStencilBuffer, nullptr, &m_pDepthStencilView));
+
+    // Create depth/stencil state.
+    D3D11_DEPTH_STENCIL_DESC depthStencilStateDesc;
+    ZeroMemory(&depthStencilStateDesc, sizeof(D3D11_DEPTH_STENCIL_DESC));
+    depthStencilStateDesc.DepthEnable = TRUE;
+    depthStencilStateDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL; // mess with this for transparency
+    depthStencilStateDesc.DepthFunc = D3D11_COMPARISON_LESS;
+    depthStencilStateDesc.StencilEnable = FALSE;
+    ThrowIfFailed(m_pDevice->CreateDepthStencilState(&depthStencilStateDesc, &m_pDepthStencilState));
+
+
+    // Create rasterizer state.
+    D3D11_RASTERIZER_DESC rasterizerDesc;
+    ZeroMemory(&rasterizerDesc, sizeof(D3D11_RASTERIZER_DESC));
+    rasterizerDesc.AntialiasedLineEnable = FALSE;
+    rasterizerDesc.CullMode = D3D11_CULL_BACK;
+    rasterizerDesc.DepthBias = 0;
+    rasterizerDesc.DepthBiasClamp = 0.0f;
+    rasterizerDesc.DepthClipEnable = TRUE;
+    rasterizerDesc.FillMode = D3D11_FILL_SOLID;
+    rasterizerDesc.FrontCounterClockwise = FALSE;
+    rasterizerDesc.MultisampleEnable = FALSE;
+    rasterizerDesc.ScissorEnable = FALSE;
+    rasterizerDesc.SlopeScaledDepthBias = 0.0f;
+    ThrowIfFailed( m_pDevice->CreateRasterizerState(&rasterizerDesc, &m_pRasterizerState));
 
     // Set viewport.
     ZeroMemory(&m_Viewport, sizeof(D3D11_VIEWPORT));
     m_Viewport.TopLeftX = 0;
     m_Viewport.TopLeftY = 0;
-    m_Viewport.Width = m_pWindow->GetWidth();
-    m_Viewport.Height = m_pWindow->GetHeight();
+    m_Viewport.Width = static_cast<FLOAT>(m_pWindow->GetWidth());
+    m_Viewport.Height = static_cast<FLOAT>(m_pWindow->GetHeight());
+    m_Viewport.MinDepth = 0.0f;
+    m_Viewport.MaxDepth = 1.0f;
     m_pDeviceContext->RSSetViewports(1, &m_Viewport);
 
     return true;
@@ -162,7 +230,7 @@ bool Graphics::SimpleRenderSetup()
     ID3D11VertexShader *pVS;
     ID3D11PixelShader *pPS;
     Filepath shaderPath;
-    Path::GenerateAssetPath(shaderPath, "shaders", "default.shader");
+    Path::GenerateAssetPath(shaderPath, "shaders", "default.hlsl");
     D3DX11CompileFromFile(shaderPath.Get(), 0, 0, "VShader", "vs_5_0", 0, 0, 0, &VS, 0, 0);
     D3DX11CompileFromFile(shaderPath.Get(), 0, 0, "PShader", "ps_5_0", 0, 0, 0, &PS, 0, 0);
 
@@ -175,30 +243,47 @@ bool Graphics::SimpleRenderSetup()
     m_pDeviceContext->PSSetShader(pPS, 0, 0);
 
 
-    // Test vertices for drawing a triangle.
-    static const int NumVerts = 3;
-    Vertex testVerts[NumVerts] =
-    {
-        Vertex(0.0f,    0.5f, 0.0f, 1.0f, 0.0f, 0.0f),
-        Vertex(0.45f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f),
-        Vertex(-0.45f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f)
-    };
+    // TSET: vertices for drawing a triangle.
+    //static const int NumVerts = 3;
+    //Vertex testVerts[NumVerts] =
+    //{
+    //    Vertex(0.0f,    0.5f, 0.0f, 1.0f, 0.0f, 0.0f),
+    //    Vertex(0.45f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f),
+    //    Vertex(-0.45f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f)
+    //};
+
+    // TEST: load a mesh.
+
+    Mesh myCube;
+    myCube.LoadContent("cube.mesha");
 
     // Create vertex buffer.
+    //D3D11_BUFFER_DESC bufferDesc;
+    //ZeroMemory(&bufferDesc, sizeof(D3D11_BUFFER_DESC));
+    //bufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+    //bufferDesc.ByteWidth = sizeof(Vertex) * NumVerts;
+    //bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    //bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+    //ThrowIfFailed(m_pDevice->CreateBuffer(&bufferDesc, NULL, &m_pVertexBuffer));
     D3D11_BUFFER_DESC bufferDesc;
     ZeroMemory(&bufferDesc, sizeof(D3D11_BUFFER_DESC));
     bufferDesc.Usage = D3D11_USAGE_DYNAMIC;
-    bufferDesc.ByteWidth = sizeof(Vertex) * NumVerts;
+    bufferDesc.ByteWidth = sizeof(Vertex) * myCube.GetNumVertices();
     bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
     bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-    ThrowIfFailed(m_pDevice->CreateBuffer(&bufferDesc, NULL, &m_pVertexBuffer));
+
+    D3D11_SUBRESOURCE_DATA resourceData;
+    ZeroMemory(&resourceData, sizeof(D3D11_SUBRESOURCE_DATA));
+    resourceData.pSysMem = &myCube.GetVertices()[0];
+    ThrowIfFailed(m_pDevice->CreateBuffer(&bufferDesc, &resourceData, &m_pVertexBuffer));
 
     // Fill the vertex buffer.
     D3D11_MAPPED_SUBRESOURCE mappedSubrcs; // information about buffer once mapped, including location of the buffer.
     ThrowIfFailed(m_pDeviceContext->Map(m_pVertexBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &mappedSubrcs));
-    memcpy(mappedSubrcs.pData, testVerts, sizeof(testVerts));
+    memcpy(mappedSubrcs.pData, &myCube.GetVertices()[0], sizeof(Vertex) * myCube.GetNumVertices());
     m_pDeviceContext->Unmap(m_pVertexBuffer, NULL);
 
+    // TODO: index buffer.
 
     // Create input layout.
     m_pDevice->CreateInputLayout(Vertex::InputDesc, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &m_pInputLayout);
--- src/CowByteEngine 0.2/Render/Graphics.h
+++ src/CowByteEngine 0.2/Render/Graphics.h
@@ -15,6 +15,7 @@
 #include "MeshManager.h"
 
 class Window;
+class Camera;
 
 struct GraphicsData : SystemData
 {
@@ -33,22 +34,46 @@ public:
     virtual bool Update(GameContext& context);
     virtual bool ShutDown();
     bool OnRender();
+    //void SetCamera();
 
 private:
     bool InitializePipeline();
     bool SimpleRenderSetup(); // TODO: Get rid of this placeholder code piece.
 
     static const int NumOfBuffers = 2;
+    enum ConstantBufferType
+    {
+        CBUFFER_APPLICATION,
+        CBUFFER_FRAME,
+        CBUFFER_OPJECT,
+        NUM_CBUFFER
+    };
 
+    // View port and other large member:
     D3D11_VIEWPORT m_Viewport;
+
+    // Ptr to the necessary three:
     ID3D11Device *m_pDevice;
     ID3D11DeviceContext *m_pDeviceContext;
     IDXGISwapChain *m_pSwapChain;
+
+    // Other graphics:
     ID3D11RenderTargetView *m_pRenderTarget;
+    ID3D11DepthStencilView* m_pDepthStencilView;
+
     ID3D11Buffer *m_pVertexBuffer;
+    ID3D11Buffer *m_pConstantBuffers[NUM_CBUFFER];
+    ID3D11Texture2D* m_pDepthStencilBuffer;
     ID3D11InputLayout *m_pInputLayout;
+    ID3D11DepthStencilState* m_pDepthStencilState;
+    ID3D11RasterizerState* m_pRasterizerState;
+
+    // Other members:
     Window *m_pWindow;
     MeshManager *m_pMeshManager;
+    Camera *m_pMainCamera;
+
+
 };
 
 #endif
