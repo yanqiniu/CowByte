9f23cd0fc7dbbf583fc9e7cfb94c082a5c09f983
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-14 19:13:01 -0700

    Assignment operator and default constructor needed after all for RefConutPtr.

--- src/CowByteEngine 0.2/Utils/CBRefCountPtr.h
+++ src/CowByteEngine 0.2/Utils/CBRefCountPtr.h
@@ -14,25 +14,35 @@ template <typename DataT>
 class CBRefCountPtr
 {
 public:
+    CBRefCountPtr();
     explicit CBRefCountPtr(DataT* ptr);
     CBRefCountPtr(const CBRefCountPtr& toCopy);
     ~CBRefCountPtr();
+    CBRefCountPtr<DataT> & operator=(const CBRefCountPtr<DataT>& other);
+
 
     DataT& operator*();
     DataT* operator->();
     DataT* Get() const;
 
 private:
-    // Disallow assignment operator to avoid complicating it...
-    CBRefCountPtr & operator=(const CBRefCountPtr& other);
 
     CBRefCountPtr_ControlBlock *m_pControlBlock;
     DataT *m_pData; // Actual pointer to the object.
 };
 
 template <typename DataT>
+CBRefCountPtr<DataT>::CBRefCountPtr() :
+    m_pControlBlock(nullptr),
+    m_pData(nullptr)
+{
+
+}
+
+template <typename DataT>
 CBRefCountPtr<DataT>::CBRefCountPtr(DataT* ptr)
 {
+    m_pData = ptr;
     m_pControlBlock = new CBRefCountPtr_ControlBlock();
     m_pControlBlock->RefCount = 1;
 }
@@ -46,8 +56,23 @@ CBRefCountPtr<DataT>::CBRefCountPtr(const CBRefCountPtr& toCopy)
     ++(m_pControlBlock->RefCount);
 }
 
+// Shallow assignment operator that does not check if resources has to be 
+// released. Because there's no way of knowing whether the object has been
+// initialized. So destruct the object before assignment explicitly if you 
+// know it's not empty.
 template <typename DataT>
-CBRefCountPtr<DataT>::~CBRefCountPtr()
+CBRefCountPtr<DataT> & CBRefCountPtr<DataT>::operator=(const CBRefCountPtr<DataT>& other)
+{
+    m_pData = other.m_pData;
+    m_pControlBlock = other.m_pControlBlock;
+    ++(m_pControlBlock->RefCount);
+
+    return *this;
+}
+
+
+template <typename DataT>
+CBRefCountPtr<DataT>::~CBRefCountPtr<DataT>()
 {
     --(m_pControlBlock->RefCount);
 
