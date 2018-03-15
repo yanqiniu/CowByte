2e6f1d592be9cc4c3fe0a170e94058660fefa162
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-15 01:14:05 -0700

    Disable Game camera. This will be added back in using a message.

--- src/CowByteEngine 0.2/Core/Game.cpp
+++ src/CowByteEngine 0.2/Core/Game.cpp
@@ -21,10 +21,10 @@ Game::~Game()
 // Call this after systems have been initialized.
 bool Game::Initialize()
 {
-    // Initialize Camera.
-    m_pMainCamera = new Camera();
-    SceneNode *pCameraSceneNode = SceneNode::CreateSceneNodeThenAttach(&SceneNode::RootNode);
-    m_pMainCamera->AttachTo_SceneNode_Parent(pCameraSceneNode);
+    //// Initialize Camera.
+    //m_pMainCamera = new Camera();
+    //SceneNode *pCameraSceneNode = SceneNode::CreateSceneNodeThenAttach(&SceneNode::RootNode);
+    //m_pMainCamera->AttachTo_SceneNode_Parent(pCameraSceneNode);
 
 
     // TEMP: Create Mesh in the scene.
@@ -42,7 +42,7 @@ bool Game::Initialize()
 
 bool Game::Update(const GameContext &context)
 {
-    m_pMainCamera->UpdateWToCMatrix();
+    //m_pMainCamera->UpdateWToCMatrix();
 
     Vec3 translateAmount(0.5f, 0, 0);
     translateAmount *= context.dTime;
