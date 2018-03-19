722124b4e526c0a2fd1660d65938f61a7549f615
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-18 22:06:07 -0700

    Quaternion multiplication.

--- src/CowByteEngine 0.2/Math/Quaternion.h
+++ src/CowByteEngine 0.2/Math/Quaternion.h
@@ -10,7 +10,9 @@
 //////////////////////////////////////////////////////////////////////////
 /*
 
-CBE SIMD fast quaternion
+CBE SIMD fast quaternion.
+
+Notice that CBE only works with normalized quat for simplicity and speed!
 
 */
 //////////////////////////////////////////////////////////////////////////
@@ -25,6 +27,7 @@ __declspec(align(16)) struct Quaternion
         float  _q[4];
     };
 
+#pragma region Con/Destructor, Getter/Setter
     // Default constructor that does nothing.
     __forceinline Quaternion() {}
 
@@ -34,6 +37,89 @@ __declspec(align(16)) struct Quaternion
     }
 
     Quaternion(const Vec3 &axis, float angleInRad);
+
+    __forceinline Quaternion(const __m128 &inData) :
+        _data(inData)
+    {
+    }
+#pragma endregion
+
+#pragma region Common operations
+
+    __forceinline Quaternion Conjugate()
+    {
+        return Quaternion(_mm_mul_ps(_data, _mm_setr_ps(-1.0f, -1.0f, -1.0f, 1.0f)));
+    }
+
+    // Inverse the current quat, assuming it's unit.
+    __forceinline void Inverse()
+    {
+        _data = _mm_mul_ps(_data, _mm_setr_ps(-1.0f, -1.0f, -1.0f, 1.0f));
+    }
+
+    // Return inversed version of this quat, assuming it's unit.
+    __forceinline Quaternion Inversed()
+    {
+        return Conjugate();
+    }
+
+    __forceinline float SqrLen()
+    {
+        return _mm_dp_ps(_data, _data, 0b11110001).m128_f32[0];
+    }
+
+    __forceinline float Len()
+    {
+        return _mm_sqrt_ss(_mm_dp_ps(_data, _data, 0b11110001)).m128_f32[0];
+    }
+
+    // P.Mul(Q) means a rotation Q followed by P.
+    __forceinline void Mul(const Quaternion &q)
+    {
+        __m128 qV = _CB_INSERT_SINGLE(q._data, 0, 3);
+        __m128 pV = _CB_INSERT_SINGLE(  _data, 0, 3);
+
+        __m128 vecPart = _mm_mul_ps(_mm_shuffle_ps(_data, _data, _CB_SHUFFLE_R(3, 3, 3, 3)), qV);                    // pS * qV
+        vecPart = _mm_add_ps(vecPart, _mm_mul_ps(_mm_shuffle_ps(q._data, q._data, _CB_SHUFFLE_R(3, 3, 3, 3)), pV)); // ..+ qS * pV
+
+        // Cross pV x qV
+        __m128 cross = _mm_mul_ps(_mm_shuffle_ps(pV, pV, _MM_SHUFFLE(3, 0, 2, 1)), 
+                                   _mm_shuffle_ps(qV, qV, _MM_SHUFFLE(3, 1, 0, 2)));
+        cross = _mm_sub_ps(cross, _mm_mul_ps(_mm_shuffle_ps(pV, pV, _MM_SHUFFLE(3, 1, 0, 2)),
+                                             _mm_shuffle_ps(qV, qV, _MM_SHUFFLE(3, 0, 2, 1))));
+        vecPart = _mm_add_ps(vecPart, cross); // .. + pV x qV
+
+        __m128 scalarPart = _mm_dp_ps(_data, _mm_mul_ps(q._data, _mm_setr_ps(-1.0f, -1.0f, -1.0f, 1.0f)), 0b11111000); //(pS * qS - pV . qV)
+
+        _data = _mm_add_ps(vecPart, scalarPart);
+    }
+
+#pragma endregion
+
+#pragma region Common Operators
+
+    __forceinline void operator*=(const Quaternion &q)
+    {
+        Mul(q);
+    }
+
+    __forceinline Quaternion operator*(const Quaternion &q)
+    {
+        Quaternion toRet = (*this);
+        toRet.Mul(q);
+        return toRet;
+    }
+
+
+#pragma endregion
+
+
+
+
+    // Conjugate of unit quaternion.
+
+
+
 };
 
 #endif
--- src/Tests/Test.cpp
+++ src/Tests/Test.cpp
@@ -21,8 +21,12 @@ int main()
 
     Vec3 axix(Vec3(3.33f, 4.44f, 6345.f));
     axix.Normalize();
-    Quaternion q(axix, 1.234f);
+    Quaternion p(1.2f, 3.34f, 6.4f, 23.0f);
+    Quaternion q(3.21f, 7.34f, 11.5f, 2.1f);
 
+    Quaternion r = p*q;
+
+    q.Inverse();
     //////////////////////////////////////////////////////////////////////////
     double duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
     printf("My duration: %f\n", duration);
