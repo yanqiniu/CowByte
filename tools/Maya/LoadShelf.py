48081db4200d64312dd1b22f8ae7800f2dea2e95
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-22 01:15:14 -0700

    First mesh successfully ported from Maya. The streamlined expoerter will be tomorrow's task.

--- tools/Maya/LoadShelf.py
+++ tools/Maya/LoadShelf.py
@@ -1,26 +1,22 @@
-# Load CowByte Engine custom Maya tool shelf.
-
-import importlib
 import maya.cmds as mc
+from CowShelf import _shelf
 
-shelf = input('ShelfBase.py')
-importlib.import_module(shelf)
 
-# null placeholder
-def _null(*args):
-    pass
+maya_tools_path = os.path.dirname(os.path.realpath(__file__))
 
+def PrintPath():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+print(maya_tools_path)
 
 class Shelf(_shelf):
+    def __init__(self,name):
+        _shelf.__init__(self, name, "")
+
     def build(self):
         self.addButon(label="button1")
         self.addButon("button2")
-        self.addButon("popup")
-        p = mc.popupMenu(b=1)
-        self.addMenuItem(p, "popupMenuItem1")
-        self.addMenuItem(p, "popupMenuItem2")
-        sub = self.addSubMenu(p, "subMenuLevel1")
-        self.addMenuItem(sub, "subMenuLevel1Item1")
-        sub2 = self.addSubMenu(sub, "subMenuLevel2")
 
-s = Shelf()
+PrintPath()
+# s = Shelf("CowByteEngine")
+
