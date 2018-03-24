c6bdbf24da8382b6511a225f7f7944d0b9aae044
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-24 03:28:09 -0700

    Adding Vec2 class.

---
+++ src/CowByteEngine 0.2/Math/Vec2.h
@@ -0,0 +1,36 @@
+#ifndef _VEC2_H
+#define _VEC2_H
+
+#include "../Utils/typedefs.h"
+
+struct Vec2
+{
+    FLOAT32 X;
+    FLOAT32 Y;
+
+    Vec2() :
+        X(0),
+        Y(0)
+    {
+
+    }
+
+    Vec2(float inX, float inY) :
+        X(inX), Y(inY)
+    {
+
+    }
+
+    friend bool operator==(const Vec2& p1, const Vec2& p2)
+    {
+        return p1.X == p2.X && p1.Y == p2.Y;
+    }
+
+    friend bool operator!=(const Vec2& p1, const Vec2& p2)
+    {
+        return p1.X != p2.X || p1.Y != p2.Y;
+    }
+
+};
+
+#endif
