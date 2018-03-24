b214449e7eecac0a3385cf79fc12c2cf66f35540
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-24 03:29:26 -0700

    Adding Texcoord to vertex.

---
+++ src/CowByteEngine 0.2/Render/Vertex.h
@@ -0,0 +1,23 @@
+#ifndef _VERTEX_H
+#define _VERTEX_H
+
+#include <d3d11.h>
+
+#include "../Math/Vec3.h"
+#include "../Math/Vec2.h"
+
+
+struct Vertex
+{
+    Vertex();
+    ~Vertex();
+
+    static D3D11_INPUT_ELEMENT_DESC InputDesc[3];
+    Vec3 m_Pos;
+    Vec3 m_Normal;
+    Vec2 m_TexCoord;
+};
+
+
+#endif // 
+
