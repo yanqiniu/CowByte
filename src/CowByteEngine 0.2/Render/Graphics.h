492a900b92f1f4103300ab5c294513e7d32498b9
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-23 21:24:58 -0700

    Removed reference to 2010 DX11 SDK.
    Not needed with windows kits.

--- src/CowByteEngine 0.2/Render/Graphics.h
+++ src/CowByteEngine 0.2/Render/Graphics.h
@@ -3,14 +3,10 @@
 
 #include <windows.h>
 #include <windowsx.h>
-#include <d3d11.h>
-#include <D3DX10.h>
-#include <D3DX11.h>
+#include <d3d11_1.h>
 #include <d3dcompiler.h>
 
 #pragma comment (lib, "d3d11.lib")
-#pragma comment (lib, "d3dx11.lib")
-#pragma comment (lib, "d3dx10.lib")
 #pragma comment (lib, "d3dcompiler.lib")
 
 #include "../Core/System.h"
