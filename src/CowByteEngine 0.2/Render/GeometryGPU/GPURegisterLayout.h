17b3b02a2d1e3f34f221261fb34eef7188a7d2f0
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-26 17:53:25 -0700

    Add Material CPU and GPURegisterLayout.

---
+++ src/CowByteEngine 0.2/Render/GeometryGPU/GPURegisterLayout.h
@@ -0,0 +1,13 @@
+#ifndef _GPU_REGISTER_LAYOUT_H
+#define _GPU_REGISTER_LAYOUT_H
+
+enum GPUConstants
+{
+    PerApplication = 0,
+    PerFrame = 1,
+    PerObject = 2, 
+
+    PerMat = 3,
+};
+
+#endif // 
