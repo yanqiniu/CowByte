a5afbc343c3966ddaca0b280ca70f1dda96a89ca
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-27 14:53:07 -0700

    HK: clean up Component.

--- src/CowByteEngine 0.2/Core/Component.h
+++ src/CowByteEngine 0.2/Core/Component.h
@@ -18,38 +18,37 @@ public:
     Component();
     virtual ~Component();
 
-    void AcceptMessage(CBRefCountPtr<Message> &pMsg);
-
+    // Basic:
     virtual bool Initialize();
     virtual bool Shutdown();
-    bool UpdateTree(const GameContext &context); // A "larger" update function that calls Update on this and offspring.
-    void SetActive(bool inBool);
-    bool IsActiveSelf();
-    size_t GetNumOffsprings() const { return m_nOffsprings; };
+    bool   UpdateTree(const GameContext &context); // A "larger" update function that calls Update on this and offspring.
+    void   SetActive(bool inBool);
+    bool   IsActiveSelf();
 
-
-    void AttachTo_NonSceneNode_Parent(Component* parentPtr);
-    void AttachTo_SceneNode_Parent(SceneNode* parentPtr);
-    bool HasSceneNodeParent();
+    // Hierarchy and messaging related:
+    size_t     GetNumOffsprings() const { return m_nOffsprings; };
+    void       AttachTo_NonSceneNode_Parent(Component* parentPtr);
+    void       AttachTo_SceneNode_Parent(SceneNode* parentPtr);
+    bool       HasSceneNodeParent();
     SceneNode *GetParentSceneNode() const;
-    void HandleMessagesQueueTree();
-
-
+    void       AcceptMessage(CBRefCountPtr<Message> &pMsg);
+    void       HandleMessagesQueueTree();
 
 protected:
     virtual bool Update(const GameContext &context) = 0;
     virtual void _HandleMessage(CBRefCountPtr<Message> &pMsg) = 0;
-    void HandleMessageQueue();
-    static void IncreNumOffsprings(Component* comp, size_t amount);
+    void         HandleMessageQueue();
 
     CBQueue<CBRefCountPtr<Message>> m_MessageQueue;
-    CBVector<Component*> m_Components;
-    Component *m_pParentComponent;
-    bool m_bParentIsSceneNode;
+    CBVector<Component*> m_ChildrenComps;
+    Component *m_pParentComp;
 
 private:
     bool AddChild(Component* childPtr);
+    void IncreNumOffsprings(size_t amount);
+
     size_t m_nOffsprings; // Total number of nodes below this one.
+    bool m_bParentIsSceneNode;
     bool m_bIsActive;
 };
 
