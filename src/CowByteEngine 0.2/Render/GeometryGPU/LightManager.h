2b7b0dfefd077a91a7b7a3bbec23f2896bf106ad
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-28 18:07:41 -0700

    Add LightManager.

---
+++ src/CowByteEngine 0.2/Render/GeometryGPU/LightManager.h
@@ -0,0 +1,24 @@
+#ifndef _LIGHT_MANAGER_H
+#define _LIGHT_MANAGER_H
+#include "../../Core/Component.h"
+#include "../../Utils/CBVector.h"
+#include <d3d11_1.h>
+
+class Light;
+// This should be attached to Graphics.
+class LightManager : public Component
+{
+public:
+    LightManager();
+    ~LightManager();
+    virtual bool Update(const GameContext &context) override;
+    virtual void _HandleMessage(CBRefCountPtr<Message> &pMsg) override;
+    bool UpdateLightsGPU(ID3D11DeviceContext *pDeviceContext);
+    bool CreateLightsGPU(ID3D11Device *pDevice, ID3D11DeviceContext *pDeviceContext);
+private:
+    CBVector<Light*> m_CPULights;
+    CBVector<ID3D11Buffer*> m_GPULights;
+};
+
+#endif
+
