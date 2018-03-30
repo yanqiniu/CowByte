165c848aeeaaf64794b32a60e0cb0c087dc23972
A calm cow <yanqi.roy.niu@gmail.com>
2018-03-30 00:43:24 -0700

    Update demo code and screenshot.

--- assets/shaders/NormalMapped_ps.hlsl
+++ assets/shaders/NormalMapped_ps.hlsl
@@ -45,6 +45,7 @@ float4 PShader(PS_Input input) : SV_TARGET
     // Transform sampled normal to tangent space.
     float4x4 TBN = float4x4(input.tangent, input.normal, input.bitangent,  float4(0.0f, 0.0f, 0.0f, 1.0f));
     normalSampled = mul(normalSampled, TBN); 
+    normalSampled = normalize(normalSampled);
 
     // Apply normal and calculate lighting.
     input.normal = float4(normalSampled.x, normalSampled.z, normalSampled.y, 0.0f); // Switch y and z because we are using B value as Y.
