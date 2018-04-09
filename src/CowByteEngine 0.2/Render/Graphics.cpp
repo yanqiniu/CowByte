e52bdb1f299f7edcf300f61d5435553365ebd432
A calm cow <yanqi.roy.niu@gmail.com>
2018-04-08 22:50:35 -0700

    Enable debug layer.

--- src/CowByteEngine 0.2/Render/Graphics.cpp
+++ src/CowByteEngine 0.2/Render/Graphics.cpp
@@ -258,7 +258,7 @@ bool Graphics::InitializePipeline()
         nullptr,
         D3D_DRIVER_TYPE_HARDWARE,
         nullptr,
-        0,
+        D3D11_CREATE_DEVICE_DEBUG,
         featureLevels,
         _countof(featureLevels),
         D3D11_SDK_VERSION,
