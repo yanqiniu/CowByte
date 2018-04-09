5deb0bfb999f8332839be4b0fba54759b842a04f
A calm cow <yanqi.roy.niu@gmail.com>
2018-04-08 23:16:50 -0700

    Clean up depth map test shader.

--- assets/shaders/default_ZBuffered_ps.hlsl
+++ assets/shaders/default_ZBuffered_ps.hlsl
@@ -12,13 +12,5 @@ float4 main(PS_Input input) : SV_TARGET
     if(input.depthPosition.z - sampledZ > 0.02) // threshhold for MSAA
         return float4(0.0f, 1.0f, 0.0f, 1.0f);
 
-    input.normal = normalize(input.normal);
-    input.tangent = normalize(input.tangent);
-    input.bitangent = normalize(input.bitangent);
-
-
-    float4 sampleColor = gAlbedoMap.Sample(gAlbedoSS, input.texcoord);
-    //float4 outColor = float4(sample, 0.0f, 0.0f, 1.0f);
-    //outColor *= saturate(PhongLighting(m_Light0, input) + PhongLighting(m_Light1, input));// + PhongLighting(m_Light2, input) + PhongLighting(m_Light3, input));
-    return sampleColor;//ZBufferTest(input);
+    return gAlbedoMap.Sample(gAlbedoSS, input.texcoord);
 }
